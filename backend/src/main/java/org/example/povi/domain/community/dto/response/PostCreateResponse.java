b8af108adf7c89cd2253120558d23b605458a060
yenazigeumena <gogguu@bu.ac.kr>
2025-10-22 17:53:10 +0900

    fix: 어노테이션 삭제

--- backend/src/main/java/org/example/povi/domain/community/dto/response/PostCreateResponse.java
+++ backend/src/main/java/org/example/povi/domain/community/dto/response/PostCreateResponse.java
@@ -2,9 +2,9 @@ package org.example.povi.domain.community.dto.response;
 
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
+
+
 public record PostCreateResponse (
     Long postId,
     String message
