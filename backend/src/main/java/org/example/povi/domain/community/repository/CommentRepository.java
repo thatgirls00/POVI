21e15b0c9c97e3179c1163b8aac480aa2428be78
yenazigeumena <gogguu@bu.ac.kr>
2025-10-22 15:10:47 +0900

    feat: 댓글 조회 기능 추가

--- backend/src/main/java/org/example/povi/domain/community/repository/CommentRepository.java
+++ backend/src/main/java/org/example/povi/domain/community/repository/CommentRepository.java
@@ -1,8 +1,10 @@
 package org.example.povi.domain.community.repository;
 
 import org.example.povi.domain.community.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    Page<Comment> findByUserId(Long userId, Pageable pageable);
 }
